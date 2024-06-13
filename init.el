4db5d7d4debf4271ff8e9775146fc1548d23fdcb
zijian yi <zijianyi66@gmail.com>
2024-06-13 00:06:33 -0500

    more basic setup

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (setq mac-option-modifier 'meta
       mac-command-modifier 'super)
 
@@ -8,11 +9,19 @@
 (global-set-key (kbd "s-z") 'undo) ;对应Windows上面的Ctrol-z 撤销
 (global-set-key (kbd "s-x") 'kill-region) ;对应Windows上面的Ctrol-x 剪切
 
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-(setq tab-always-indent 'complete)
-(icomplete-mode 1)
+(global-display-line-numbers-mode 1)
 
-(set-face-attribute 'default nil :height 140)
+(setq inhibit-splash-screen 1)
+
+(set-face-attribute 'default nil :family "JetBrainsMono Nerd Font" :height 160)
+
+;; open init file with f2
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-init-file)
 
 ;;(use-package evil
 ;;  :ensure t
@@ -33,8 +42,28 @@
   :config
   (vertico-mode 1))
 
+(use-package magit
+  :ensure t)
+
+(use-package company
+  :ensure t)
+
 (load-theme `tango)
 
 (use-package auctex
   :ensure t
   :defer t)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+(global-company-mode 1)
+(setq company-minimum-prefix-length 1)
+(setq company-idle-delay 0)
+
+(global-set-key (kbd "C-h C-f") 'find-function)
+
+;; (use-package rust-ts-mode
+;;   :mode ("\\.rs\\'" . rust-ts-mod)
+;;   :hook ((rust-ts-mode . eglot)
+;; 	 (rust-ts-mode . company-mode)))
